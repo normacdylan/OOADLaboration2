73c45a12b093d90ed626365539e2a88a6b5ebef2
logic93 <jean.melki@infotiv.se>
2018-12-11 21:30:07 +0100

    Small adjustments

--- OOADLaboration2/ViewModels/SearchViewModel.cs
+++ OOADLaboration2/ViewModels/SearchViewModel.cs
@@ -9,13 +9,57 @@ namespace OOADLaboration2.ViewModels
     {
         public ValidatableObject<string> Input { get; set; } = new ValidatableObject<string>();
         string errorMessage;
+        int selectedIndex;
+        string entry;
+        string[] types;
+        ICommand searchCommand;
+        INavigation Navigation;
+
+        public SearchViewModel(INavigation Navigation)
+        {
+            entry = "";
+            selectedIndex = 0;
+            this.Navigation = Navigation;
+            Types = new string[] { "music", "movies", "shows", "books", "authors", "games" };
+            SearchCommand = new Command(Search);
+            AddValidations();
+        }
+
+        public string[] Types
+        {
+            get => types;
+            set => SetProperty(ref types, value: value);
+        }
+
+        public string Entry
+        {
+            get => entry;
+            set => SetProperty(ref entry, value: value);
+        }
+
+        public int SelectedIndex
+        {
+            get => selectedIndex;
+            set => SetProperty(ref selectedIndex, value: value);
+        }
+
+        public ICommand SearchCommand
+        {
+            get => searchCommand;
+            private set => SetProperty(ref searchCommand, value);
+        }
+
+        async void Search()
+        {
+            await Navigation.PushAsync(new ResultPage(entry, Types[selectedIndex]));
+        }
 
         void SearchPressed()
         {
-            ValidateSearchInput();
+            ValidateSearch();
         }
 
-        public string ErrorMessage
+        public string Error
         {
             get => errorMessage;
             set => SetProperty(ref errorMessage, value);
@@ -31,52 +75,7 @@ namespace OOADLaboration2.ViewModels
 
         bool ValidateSearch()
         {
-            return SearchInput.Validate();
-        }
-        
-        private int selectedIndex;
-        private string entry;
-        private string[] types;
-        private ICommand searchCommand;
-        INavigation Navigation;
-        public SearchViewModel(INavigation Navigation)
-        {
-            entry = "";
-            selectedIndex = 0;
-            this.Navigation = Navigation;
-            Types = new string[] { "music", "movies", "shows", "books", "authors", "games" };
-            SearchCommand = new Command(Search);
-            AddValidations();
-        }
-
-        public string[] Types
-        {
-            get => types;
-            set => SetProperty(storage: ref types, value: value);
-        }
-
-        public string Entry
-        {
-            get => entry;
-            set => SetProperty(storage: ref entry, value: value);
-        }
-
-        public int SelectedIndex
-        {
-            get => selectedIndex;
-            set => SetProperty(storage: ref selectedIndex, value: value);
-        }
-
-        public ICommand SearchCommand
-        { 
-            get => searchCommand;
-            private set => SetProperty(storage: ref searchCommand, value: value); 
-        }
-
-        async void Search()
-        {   
-            if (ValidateSearchInput());
-              await Navigation.PushAsync(new ResultPage(entry, Types[selectedIndex]));
+            return Input.Validate();
         }
 
     }
